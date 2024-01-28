f4bac65571e8d3c1f104d7c0466c4f596f9d1029
Abel Salgado Romero <abelromero@gmail.com>
2024-01-28 22:39:09 +0100

    Add DemoRunner class to JCommander example (#49)
    Allows creating a simple binary to compare features and binary size with Picocli

--- java-modules/cli-args-jcommander/src/main/java/org/abelsromero/demo/cli/impl/DemoRunner.java
+++ java-modules/cli-args-jcommander/src/main/java/org/abelsromero/demo/cli/impl/DemoRunner.java
@@ -6,7 +6,7 @@ import org.abelsromero.demo.cli.CliOptionsHandler;
 public class DemoRunner {
 
     public static void main(String[] args) {
-        CliOptionsHandler optionsHandler = new PicocliOptionsHandler();
+        CliOptionsHandler optionsHandler = new JCommanderOptionsHandler();
         CliOptions options = optionsHandler.parse(args);
 
         System.out.println("Name: " + options.getName());
