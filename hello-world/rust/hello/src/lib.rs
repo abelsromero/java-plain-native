c58365bc1abf043e6fe8b4898cd7fa68e3a07409
Abel Salgado Romero <abelromero@gmail.com>
2025-04-06 11:10:24 +0200

    Minor idiomatic Rust refactor (#85)

--- hello-world/rust/hello/src/lib.rs
+++ hello-world/rust/hello/src/lib.rs
@@ -3,9 +3,5 @@ use std::env;
 pub fn extract_text() -> String {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() == 1 {
-        return String::new();
-    } else {
-        return String::from(&args[1]);
-    }
+    args.get(1).cloned().unwrap_or_default()
 }
